c56c25206a815e36fc4b179058a351833e31616b
Katya982 <xamko98@gmail.com>
2024-02-25 11:09:02 -0500

    add

--- src/pages/FavCarsPage.jsx
+++ src/pages/FavCarsPage.jsx
@@ -1,27 +1,43 @@
-import { Filter } from "components/Filter/Filter"
-import React, { useState, useEffect } from 'react'
-import { GalleryView } from '../components/CarsGallery/CarsGallery.styled'
-import { OneGalleryItem } from '../components/OneGalleryItem/OneGalleryItem'
-import datacars from '../components/CarsGallery/cars.json'
+import { Filter } from "components/Filter/Filter";
+import React, { useState, useEffect } from 'react';
+import { GalleryView } from '../components/CarsGallery/CarsGallery.styled';
+import { OneGalleryItem } from '../components/OneGalleryItem/OneGalleryItem';
+import { fetchDataFavorite } from '../components/api'; 
 
 export const FavCars = () => {
-    const [favoriteCars, setFavoriteCars] = useState([])
-    
-    useEffect(() => {
-        const storedFavorites = JSON.parse(localStorage.getItem('favorites')) || []
+    const [favoriteCars, setFavoriteCars] = useState([]);
+    const [datacars, setDataCars] = useState([]);
+
+    const updateFavoriteCars = () => {
+        const storedFavorites = JSON.parse(localStorage.getItem('favorites')) || [];
         setFavoriteCars(storedFavorites);
-    }, [favoriteCars])
+    };
+
+    useEffect(() => {        
+        updateFavoriteCars();
+        
+        const fetchDataFromAPI = async () => {
+            try {
+                const data = await fetchDataFavorite(); 
+                setDataCars(data); 
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+
+        fetchDataFromAPI()
+    }, []);
 
     return (
         <>
-            <Filter />            
-            <GalleryView>                   
-                    {datacars
-                        .filter(car => favoriteCars.includes(car.id))
-                        .map((car) => (
-                            <OneGalleryItem data={car} key={car.id} />
-                        ))}
-            </GalleryView>            
+            <Filter />
+            <GalleryView>
+                {datacars
+                    .filter(car => favoriteCars.includes(car.id))
+                    .map((car) => (
+                        <OneGalleryItem data={car} key={car.id} />
+                    ))}
+            </GalleryView>
         </>
     );
 };
